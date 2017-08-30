850b003632b26d75587cad3b71be1c8782555dc8
Drew Bagwell <abagwe7359@greenvilleschools.us>
2017-08-30 10:30:39 -0400

    Created some functions

--- Geometry.java
+++ Geometry.java
@@ -25,6 +25,31 @@ public class Geometry
         return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
     }
     
+    public static double parallelogramArea(double length, double width) 
+    {
+        return length * width;
+    }
+
+    public static double trapezoidArea(double baseone, double basetwo, double height) 
+    {
+        return (baseone + basetwo) * height / 2.0;
+    }
+
+    public static double rectangularPrismVolume(double length, double width, double height) 
+    {
+        return length * width * height;
+    }
+
+    public static double coneVolume(double radius, double height) 
+    {
+        return Math.PI * height * Math.pow(radius, 2) / 3.0;
+    }
+
+    public static double rectangularPrismSurfaceArea(double length, double width, double height) 
+    {
+        return 2.0 * ((width * length) + (height * length) + (height * width));
+    }
+
     public static void main(String[] args) 
     {
         double ta1 = triangleArea(4.2, 8.1);
@@ -35,5 +60,20 @@ public class Geometry
         
         double ra1 = rectangleArea(5.2, 9.3);
         System.out.println(ra1);
+        
+        double pa1 = parallelogramArea(4.0, 6.0);
+        System.out.println(pa1);
+        
+        double tra1 = trapezoidArea(9.0, 13.0, 5.0);
+        System.out.println(tra1);
+        
+        double rpv1 = rectangularPrismVolume(3, 10, 6);
+        System.out.println(rpv1);
+        
+        double cv1 = coneVolume(7, 12);
+        System.out.println(cv1);
+        
+        double rpsa1 = rectangularPrismSurfaceArea(5.0, 9.0, 8.0);
+        System.out.println(rpsa1);
     }
 }
